6ec8b75927ee97f28bf6e854d19a09712c747103
pk-1111 <kyawkaungsan1111@gmail.com>
2026-05-18 02:40:02 +0630

    F

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,8 @@
-import BackgroundVideo from "@/components/Background/BackgroundVideo";
+
 import FloatingNavbar from "@/components/BottomNavbar/BottomNavbar";
-import ContactPage from "@/components/GetInTouch/GetInTouch";
+
 import Profile from "@/components/Profile/Profile";
-import VisitPage from "@/components/VisitPage/VisitPage";
-import { main } from "framer-motion/client";
+
 
 export default function Page() {
     return (
